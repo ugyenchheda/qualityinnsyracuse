a73c6549dd4128354ba21b8b540372737d445e65
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-03-11 00:10:01 +0200

    Update author-bio.php

--- author-bio.php
+++ author-bio.php
@@ -1,11 +1,5 @@
 <?php
-/**
- * The template for displaying Author bios
- *
- * @package WordPress
- * @subpackage Twenty_Fifteen
- * @since Twenty Fifteen 1.0
- */
+
 ?>
 
 <div class="author-info">
