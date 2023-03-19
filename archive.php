754a15874361a12c5d8c85361243097251b42098
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-03-19 16:55:43 +0200

    Update archive.php

--- archive.php
+++ archive.php
@@ -16,7 +16,6 @@ get_header(); ?>
 			</header><!-- .page-header -->
 
 			<?php
-			// Start the Loop.
 			while ( have_posts() ) : the_post();
 
 				/*
@@ -26,17 +25,13 @@ get_header(); ?>
 				 */
 				get_template_part( 'content', get_post_format() );
 
-			// End the loop.
 			endwhile;
 
-			// Previous/next page navigation.
 			the_posts_pagination( array(
 				'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
 				'next_text'          => __( 'Next page', 'twentyfifteen' ),
 				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
 			) );
-
-		// If no content, include the "No posts found" template.
 		else :
 			get_template_part( 'content', 'none' );
 
