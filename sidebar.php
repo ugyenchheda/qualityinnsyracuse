b5e1784358019df4faca5aed35da5a581f903ee8
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-03-19 16:56:20 +0200

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * The sidebar containing the main widget area
- *
- * @package WordPress
- * @subpackage Twenty_Fifteen
- * @since Twenty Fifteen 1.0
- */
-
 if ( has_nav_menu( 'primary' ) || has_nav_menu( 'social' ) || is_active_sidebar( 'sidebar-1' )  ) : ?>
 	<div id="secondary" class="secondary">
 
@@ -19,7 +11,7 @@ if ( has_nav_menu( 'primary' ) || has_nav_menu( 'social' ) || is_active_sidebar(
 						'theme_location' => 'primary',
 					) );
 				?>
-			</nav><!-- .main-navigation -->
+			</nav>
 		<?php endif; ?>
 
 		<?php if ( has_nav_menu( 'social' ) ) : ?>
@@ -33,7 +25,7 @@ if ( has_nav_menu( 'primary' ) || has_nav_menu( 'social' ) || is_active_sidebar(
 						'link_after'     => '</span>',
 					) );
 				?>
-			</nav><!-- .social-navigation -->
+			</nav>
 		<?php endif; ?>
 
 		<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
