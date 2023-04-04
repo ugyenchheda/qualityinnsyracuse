241e6c096bc605aaf8963a63e0ea1e1016168276
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-04-04 23:38:40 +0300

    Update search.php

--- search.php
+++ search.php
@@ -22,13 +22,7 @@ get_header(); ?>
 			// Start the loop.
 			while ( have_posts() ) : the_post(); ?>
 
-				<?php
-				/*
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'content', 'search' );
+				<?php get_template_part( 'content', 'search' );
 
 			// End the loop.
 			endwhile;
