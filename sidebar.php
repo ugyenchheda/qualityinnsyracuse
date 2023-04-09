3ad854699738c06f170e51ed120cdfa13233d6af
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-04-09 23:47:17 +0300

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
 <?php
-if ( has_nav_menu( 'primary' ) || has_nav_menu( 'social' ) || is_active_sidebar( 'sidebar-1' )  ) : ?>
+if ( has_nav_menu( 'primary' ) || has_nav_menu( 'social' ) || is_active_sidebar( 'sidebar-2' )  ) : ?>
 	<div id="secondary" class="secondary">
 		<?php if ( has_nav_menu( 'primary' ) ) : ?>
 			<nav id="site-navigation" class="main-navigation" role="navigation">
@@ -27,7 +27,7 @@ if ( has_nav_menu( 'primary' ) || has_nav_menu( 'social' ) || is_active_sidebar(
 		<?php endif; ?>
 		<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 			<div id="widget-area" class="widget-area" role="complementary">
-				<?php dynamic_sidebar( 'sidebar-1' ); ?>
+				<?php dynamic_sidebar( 'sidebar-2' ); ?>
 			</div><!-- .widget-area -->
 		<?php endif; ?>
 	</div><!-- .secondary -->
